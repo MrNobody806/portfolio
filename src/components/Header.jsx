f854a55078f013eba6184fb3cebf737b1406af4a
MrNobody806 <manuhe839@example.com>
2025-08-28 15:03:09 +0300

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,7 +51,7 @@ const Header = ({ darkMode, toggleDarkMode }) => {
           className="text-2xl font-bold"
         >
           <span className={darkMode ? "text-blue-400" : "text-blue-600"}>
-            Manu
+            Manuhe Wolde
           </span>
         </motion.div>
 
